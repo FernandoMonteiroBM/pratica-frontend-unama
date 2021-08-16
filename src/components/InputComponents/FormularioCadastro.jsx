aaa614266de7d46c27a1e22738d96200ad4918a6
Fernando Monteiro <fernandomonteirobm@gmail.com>
2021-08-16 11:24:00 -0300

    Componentização dos campos de input

--- src/components/InputComponents/FormularioCadastro.jsx
+++ src/components/InputComponents/FormularioCadastro.jsx
@@ -11,102 +11,140 @@ function FormularioCadastro({ aoEnviar }) {
   const [cpf, setCpf] = useState("");
   const [genero, setGenero] = useState("");
   const [nascimento, setNascimento] = useState("");
+  const [tipo, setTipo] = useState("");
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        aoEnviar({ nome, sobrenome, cpf, email, emailC, genero });
-      }}
-    >
-      <div className="formulario">
-        <h1 className="input-padrao-texto">Formulário de cadastro</h1>
-        <label className="input-padrao-texto">E-mail</label>
-        <input
-          className="input-padrao"
-          value={email}
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-          id="email"
-        />
-        <label className="input-padrao-texto">Confirme seu e-mail</label>
-        <input
-          className="input-padrao"
-          value={emailC}
-          onChange={(event) => {
-            setEmailc(event.target.value);
-          }}
-          id="emailC"
-        />
-        <label className="input-padrao-texto">Nome</label>
-        <input
-          className="input-padrao"
-          value={nome}
-          onChange={(event) => {
-            setNome(event.target.value);
-          }}
-          id="nome"
-        />
-        <label className="input-padrao-texto">Sobrenome</label>
-        <input
-          className="input-padrao"
-          value={sobrenome}
-          onChange={(event) => {
-            setSobrenome(event.target.value);
-          }}
-          id="sobrenome"
-        />
-        <label className="input-padrao-texto">CPF</label>
-        <input
-          className="input-padrao"
-          value={cpf}
-          onChange={(event) => {
-            setCpf(event.target.value);
-          }}
-          id="CPF"
-        />
-      </div>
-
-      <div className="genero-nascimento">
-        <div className="formulario-botao">
-          <label className="input-padrao-texto">Genero</label>
-          <RadioButton
-            text="Masculino"
-            value="Masculino"
-            onChange={(e) => setGenero("Masculino")}
-            id="Genero"
-          />
-          <RadioButton
-            text="Feminino"
-            value="Feminino"
-            onChange={(e) => setGenero("Feminino")}
-            id="Genero"
-          />
-          <RadioButton
-            text="Outro"
-            value="Outro"
-            onChange={(e) => setGenero("Outro")}
-            id="Genero"
-          />
-        </div>
-        <div className="formulario-nascimento">
-          <label className="input-padrao-texto">Data de Nascimento</label>
-          <input
-            className="input-padrao"
-            value={nascimento}
+    <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          aoEnviar({
+            nome,
+            sobrenome,
+            cpf,
+            email,
+            emailC,
+            genero,
+            nascimento,
+            tipo,
+          });
+        }}
+      >
+        <div className="formulario">
+          <h1 className="input-padrao-texto">Formulário de cadastro</h1>
+          <CampoInput
+            id="email"
+            textLabel="Confirme seu Email *"
+            name="email"
             onChange={(event) => {
-              setNascimento(event.target.value);
+              setEmail(event.target.value);
             }}
-            id="nascimento"
+            value={email}
+            type="email"
+          />
+          <CampoInput
+            id="emailC"
+            textLabel="Confirme seu Email *"
+            name="emailC"
+            onChange={(event) => {
+              setEmailc(event.target.value);
+            }}
+            value={emailC}
+            type="email"
+          />
+          <CampoInput
+            id="nome"
+            textLabel="Nome *"
+            name="nome"
+            onChange={(event) => {
+              setNome(event.target.value);
+            }}
+            value={nome}
+            type="text"
+          />
+          <CampoInput
+            id="sobrenome"
+            textLabel="Sobrenome *"
+            name="sobrenome"
+            onChange={(event) => {
+              setSobrenome(event.target.value);
+            }}
+            value={sobrenome}
+            type="text"
+          />
+          <CampoInput
+            id="cpf"
+            textLabel="CPF *"
+            name="cpf"
+            onChange={(event) => {
+              setCpf(event.target.value);
+            }}
+            value={cpf}
+            type="text"
+            mask="999.999.999-99"
+            placeholder="___.___.___-__"
           />
         </div>
-      </div>
-      <div>
-        <button type="submit" className="submit-padrao-botao">
-          Cadastrar
-        </button>
-      </div>
-    </form>
+
+        <div className="genero-nascimento">
+          <div className="formulario-botao">
+            <label className="padrao-texto-genero">Genero</label>
+            <RadioButton
+              text="Masculino"
+              value="Masculino"
+              onChange={(e) => setGenero("Masculino")}
+              id="Genero"
+            />
+            <RadioButton
+              text="Feminino"
+              value="Feminino"
+              onChange={(e) => setGenero("Feminino")}
+              id="Genero"
+            />
+            <RadioButton
+              text="Outro"
+              value="Outro"
+              onChange={(e) => setGenero("Outro")}
+              id="Genero"
+            />
+          </div>
+          <div className="formulario-nascimento">
+            <CampoInput
+              id="nascimento"
+              textLabel="Data de Nascimento *"
+              name="nascimento"
+              onChange={(event) => {
+                setNascimento(event.target.value);
+              }}
+              value={nascimento}
+              type="text"
+              mask="99/99/99"
+              placeholder="dd/mm/aa"
+            />
+          </div>
+        </div>
+        <div className="tipo-cel-tel">
+          <div className="tipo">
+            <select
+              name="tipo"
+              id="tipo"
+              value={tipo}
+              onChange={(event) => {
+                setTipo(event.target.value);
+              }}
+            >
+              <option value="celular">Celular</option>
+              <option value="fixo">Fixo</option>
+            </select>
+          </div>
+          <div></div>
+        </div>
+        <div>
+          <button type="submit" className="submit-padrao-botao">
+            Cadastrar
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
